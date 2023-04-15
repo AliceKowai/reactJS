dcf8da60186f9a343ec3a81db7ef30d165bba84c
Alice <devalice117@gmail.com>
2023-04-15 11:37:33 -0300

    concluindo dapp

--- dapp/src/pages/create.js
+++ dapp/src/pages/create.js
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer";
 import Link from "next/link";
 import { useState } from "react";
-import { addCampaign } from "@/services/web3Service";
+import { addCampaign, getLastCampaignId } from "@/services/web3Service";
 
 function Create() {
 
@@ -15,12 +15,14 @@ function Create() {
     function btnSaveClick() {
         setMessage("Salvando... Aguarde...")
         addCampaign(campaign)
-            .then(tx => setMessage(JSON.stringify(tx)))
+            .then(tx => getLastCampaignId())
+            .then(id => setMessage(`Campanha salva com ID ${id}. Avise seus amigos e passe a eles esse numéro`))
             .catch(err => {
                 console.error(err)
                 setMessage(err.message)
             })
     }
+    
 
     return (
         <main className="flex flex-wrap items-center justify-between p-[45px] px-20">
@@ -28,10 +30,10 @@ function Create() {
                 <h1 className='text-6xl p-4 max-w-md'>Donate Crypto</h1>
                 <p className="px-4">Criar Campanha</p>
                 <div className="p-6 mt-4 h-full border border-coolGray-100 overflow-hidden rounded-md shadow-dashboard">
-                    <input type="text" placeholder="Titulo" id="titulo" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input" onChange={onInputChange}></input>
-                    <input type="text" placeholder="Descrição" id="descricao" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
-                    <input type="text" placeholder="URL imagem" id="image" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
-                    <input type="text" placeholder="URL Video" id="video" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
+                    <input type="text" placeholder="Titulo" id="title" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input" onChange={onInputChange}></input>
+                    <input type="text" placeholder="Descrição" id="description" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
+                    <input type="text" placeholder="URL do video" id="videoUrl" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
+                    <input type="text" placeholder="URL da imagem" id="imageUrl" className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal oultine-none focus:border-green-500 border boder-coolGray-200 rounded-lg shadown-input mt-4"onChange={onInputChange}></input>
                     <button className="block py-2 mt-4 px-5 text-center text-white font-bold text-sm leading-6 bg-blue-500 rounded-3xl transition duration-200" onClick={btnSaveClick}>Criar Campanha</button>
                     <Link href="/" className="block py-2 mt-4 px-11 text-center text-white font-bold text-sm leading-6 w-32 bg-slate-400 rounded-3xl transition duration-200">Voltar</Link>
                 </div>
@@ -42,6 +44,9 @@ function Create() {
             <Footer />
         </main>
     )
+
 }
+
+
 
 export default Create;
--- dapp/src/services/web3Service.js
+++ dapp/src/services/web3Service.js
@@ -3,23 +3,41 @@ import ABI from "./ABI.json"
 
 const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138"
 
-export async function doLogin(){
-    if (!window.ethereum) throw new Error("No MetaMask Found");
+export async function doLogin() {
+
+    if (!window.ethereum) throw new Error("No MetaMask found!");
 
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
-    console.log(accounts)
-    if (!accounts || !accounts.length) throw new Error("Wallet not found/ allowed")
-    localStorage.setItem("wallet", accounts[0])
-    return accounts[0]
+    if (!accounts || !accounts.length) throw new Error("Wallet not found/allowed.");
+
+    localStorage.setItem("wallet", accounts[0]);
+
+    return accounts[0];
 }
 
-function getContract(){
-    const web3 = new web3(window.ethereum)
-    return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, {from});
+function getContract() {
+    const web3 = new Web3(window.ethereum);
+    const from = localStorage.getItem("wallet");
+    return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from });
 }
 
-export async function addCampaign (campaign){
-    const contract = getContract()
+export function addCampaign(campaign) {
+    const contract = getContract();
     return contract.methods.addCampaign(campaign.title, campaign.description, campaign.videoUrl, campaign.imageUrl).send();
+}
+
+export function getLastCampaignId() {
+    const contract = getContract();
+    return contract.methods.nextId().call();
+}
+
+export function getCampaign(id) {
+    const contract = getContract();
+    return contract.methods.campaigns(id).call();
+}
+
+export function donate(id, donation) {
+    const contract = getContract();
+    return contract.methods.donate(id).send({ value: Web3.utils.toWei(donation, "ether") });
 }
